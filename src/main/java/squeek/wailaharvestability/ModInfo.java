d47629ce002aea0cbd22d75bdc97a5a3a6586cf1
314159265358x <314159265358x@gmail.com>
2025-02-15 04:29:41 +0000

    .

--- src/main/java/squeek/wailaharvestability/ModInfo.java
+++ src/main/java/squeek/wailaharvestability/ModInfo.java
@@ -3,7 +3,7 @@ package squeek.wailaharvestability;
 public final class ModInfo
 {
 	public static final String MODID = "wailaharvestability";
-	public static final String VERSION = "${version}";
+	public static final String VERSION = "1.1.13";
 	public static final String GUI_FACTORY_CLASS = "squeek.wailaharvestability.gui.config.GuiFactory";
 	public static final String NAME = "Waila Harvestability";
 }
